8ec61fc898327c4cc2688e305f5c24a9a9c16f9e
andreykireenkov <kireeshka32@gmail.com>
2023-03-16 13:45:58 +0300

    2. HQL [#6874]

---
+++ src/main/java/ru/job4j/cars/repository/UserRepository.java
@@ -0,0 +1,119 @@
+package ru.job4j.cars.repository;
+
+import lombok.AllArgsConstructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import ru.job4j.cars.model.User;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+public class UserRepository {
+    private final SessionFactory sf;
+
+    /**
+     * Сохранить в базе.
+     *
+     * @param user пользователь.
+     * @return пользователь с id.
+     */
+    public User create(User user) {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        session.save(user);
+        session.getTransaction().commit();
+        session.close();
+        return user;
+    }
+
+    /**
+     * Обновить в базе пользователя.
+     *
+     * @param user пользователь.
+     */
+    public void update(User user) {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        session.update(user);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    /**
+     * Удалить пользователя по id.
+     *
+     * @param userId ID
+     */
+    public void delete(int userId) {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        User user = new User();
+        user.setId(userId);
+        session.delete(user);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    /**
+     * Список пользователь отсортированных по id.
+     *
+     * @return список пользователей.
+     */
+    public List<User> findAllOrderById() {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        List<User> users = session.createQuery("from User", User.class).list();
+        users.sort(Comparator.comparing(User::getId));
+        session.getTransaction().commit();
+        session.close();
+        return users;
+    }
+
+    /**
+     * Найти пользователя по ID
+     *
+     * @return пользователь.
+     */
+    public Optional<User> findById(int userId) {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        var user = session.get(User.class, userId);
+        session.getTransaction().commit();
+        session.close();
+        return Optional.ofNullable(user);
+    }
+
+    /**
+     * Список пользователей по login LIKE %key%
+     *
+     * @param key key
+     * @return список пользователей.
+     */
+    public List<User> findByLikeLogin(String key) {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        List<User> users = session.createQuery("from User", User.class).list();
+        session.getTransaction().commit();
+        session.close();
+        return users;
+    }
+
+    /**
+     * Найти пользователя по login.
+     *
+     * @param login login.
+     * @return Optional or user.
+     */
+    public Optional<User> findByLogin(String login) {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        User user = (User) session.createQuery("from User where login=:login")
+                .setParameter("login", login)
+                .getSingleResult();
+        session.getTransaction().commit();
+        session.close();
+        return Optional.ofNullable(user);
+    }
+}
